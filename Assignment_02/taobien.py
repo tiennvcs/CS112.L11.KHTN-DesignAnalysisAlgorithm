9ba32a5cfb0ce39d0ef1261aff15d083deef9320
tiennvuit <tiennvuit@gmail.com>
2020-09-18 22:06:33 +0700

    add taobien.py

---
+++ Assignment_02/taobien.py
@@ -0,0 +1,15 @@
+def fibonacy(k: int):
+	if k == 0 or k == 1:
+		return 1
+
+	a = 1
+	b = 1
+	for i in range(2, k+1):
+		a, b = b, a + b
+
+	return b
+
+n, k = map(int, input().split())
+
+result = n*fibonacy(2*k)
+print(result)
