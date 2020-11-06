dda2985c97c470d4d79af1792d9fc77f5df9cd5b
KicksxNPQ <60230279+KicksxNPQ@users.noreply.github.com>
2020-11-06 19:09:54 +0700

    The final version "Phan So"

--- Assignment_08/phanso.py
+++ Assignment_08/phanso.py
@@ -1,12 +1,18 @@
 import math
+import sys
 
-a = int(input())
-b = int(input())
-c = int(input())
-d = int(input())
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+c = int(sys.stdin.readline())
+d = int(sys.stdin.readline())
 
 number_of_steps = 0
 
+if (a - b < c - d) and (a == b or c == d):
+    sys.stdout.write('0')
+    system.exit(0)
+
+number_of_steps = 0
 while not (a == c and b == d):
     a += 1
     b += 1
@@ -14,5 +20,9 @@ while not (a == c and b == d):
     a //= ucln
     b //= ucln
     number_of_steps += 1
-
-print(number_of_steps)
+    if (number_of_steps == 200000):
+        break
+if number_of_steps < 200000:
+    sys.stdout.write(str(number_of_steps))
+else:
+    sys.stdout.write('0')
